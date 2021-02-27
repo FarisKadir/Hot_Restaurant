afd8a1ee80e71a0f5ccf64f9c9b18f5fef99a0d3
Faris Kadir <faris.kadir@gmail.com>
2021-02-27 10:50:46 -0500

    created routes for displaying waiting list, res list, and creating a new res.

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 
 const express = require('express');
 const path = require('path');
+const { getMaxListeners } = require('process');
+const Reservation = require("./list");
 
 // Sets up the Express App
 
@@ -14,72 +16,60 @@ app.use(express.json());
 
 // Star Wars Characters (DATA)
 
-const characters = [
-  {
-    routeName: 'yoda',
-    name: 'Yoda',
-    role: 'Jedi Master',
-    age: 900,
-    forcePoints: 2000,
-  },
-  {
-    routeName: 'darthmaul',
-    name: 'Darth Maul',
-    role: 'Sith Lord',
-    age: 200,
-    forcePoints: 1200,
-  },
-  {
-    routeName: 'obiwankenobi',
-    name: 'Obi Wan Kenobi',
-    role: 'Jedi Master',
-    age: 55,
-    forcePoints: 1350,
-  },
-];
+const waitList = [
+    {
+        customerId: 17,
+        customerName: `Frank`,
+        customerEmail: `test@gmail.com`,
+        phoneNumber: `954-465-7228`,
+    },
+    {
+        customerId: 19,
+        customerName: `Frank`,
+        customerEmail: `test@gmail.com`,
+        phoneNumber: `954-465-7228`,
+    },
+]
+
+const resList = [
+    {
+        customerId: 17,
+        customerName: `Tom`,
+        customerEmail: `test@gmail.com`,
+        phoneNumber: `954-465-7228`,
+    },
+    {
+        customerId: 19,
+        customerName: `Jane`,
+        customerEmail: `test@gmail.com`,
+        phoneNumber: `954-465-7228`,
+    },
+]
 
 // Routes
 
-// Basic route that sends the user first to the AJAX Page
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'view.html')));
 
-app.get('/add', (req, res) => res.sendFile(path.join(__dirname, 'add.html')));
+// Displays everyone on the waitlist
+app.get('/api/waitlist', (req, res) => res.json(waitList));
 
-// Displays all characters
-app.get('/api/characters', (req, res) => res.json(characters));
+// Displays everyone on the reservation list
+app.get('/api/reserve', (req, res) => res.json(resList));
 
-// Displays a single character, or returns false
-app.get('/api/characters/:character', (req, res) => {
-  const chosen = req.params.character;
 
-  console.log(chosen);
 
-  /* Check each character routeName and see if the same as "chosen"
-   If the statement is true, send the character back as JSON,
-   otherwise tell the user no character was found */
-
-  for (let i = 0; i < characters.length; i++) {
-    if (chosen === characters[i].routeName) {
-      return res.json(characters[i]);
-    }
-  }
-
-  return res.json(false);
-});
-
-// Create New Characters - takes in JSON input
-app.post('/api/characters', (req, res) => {
+// Create New Reservation - takes in JSON input
+app.post('/api/tables', (req, res) => {
   // req.body hosts is equal to the JSON post sent from the user
   // This works because of our body parsing middleware
-  const newCharacter = req.body;
+  const newRes = req.body;
 
   // Using a RegEx Pattern to remove spaces from newCharacter
   // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-  newCharacter.routeName = newCharacter.name.replace(/\s+/g, '').toLowerCase();
-  console.log(newCharacter);
+  newRes.id = newRes.name.replace(/\s+/g, '').toLowerCase();
+  console.log(newRes);
 
-  characters.push(newCharacter);
-  res.json(newCharacter);
+  resList.push(newRes);
+  res.json(newRes);
 });
 
 // Starts the server to begin listening
